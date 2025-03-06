911dc1bfaf2559a2ddf87e15eeb7d9c4abd2b0e2
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-06 13:46:26 +0000

    Add ChangeString Project and SOlutuion

---
+++ ChangeString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "";
+            char character;
+            Console.Write("String: ");
+            text = Console.ReadLine();
+            Console.Write("Caráter: ");
+            character = Convert.ToChar(Console.ReadLine());
+
+            foreach (var charater in text)
+            {
+                if(character == charater)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(charater);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ RightSize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string Text ="";
+            Text = Console.ReadLine();
+
+            foreach (var word in Text)
+            {
+                if(word == ' ')
+                {
+                    Console.WriteLine(word);
+                }
+                else
+                {
+                    Console.Write(word);
+                }
+                
+            }
+        }
+    }
+}
