cb083a8f340e1455f436d7b531d8eeff25c714b2
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-07 09:01:10 +0000

    Add Right Size solution

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,20 +6,38 @@ namespace RightSize
     {
         private static void Main(string[] args)
         {
-            string Text ="";
+            string[] Words = new string[100];
+            string Text="";
+            int contador = 0;
             Text = Console.ReadLine();
 
             foreach (var word in Text)
             {
                 if(word == ' ')
                 {
-                    Console.WriteLine(word);
+                    contador++;
                 }
                 else
                 {
-                    Console.Write(word);
+                    Words[contador]+= word;
                 }
                 
+            }
+            foreach (var item in Words)
+            {
+                if(item.Length<=3)
+                {
+                    
+                }
+                else if(item.Length>=8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
     }
