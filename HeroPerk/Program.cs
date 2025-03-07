860687f443d41e3e2107d475137ecf7c5bd4e817
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-07 13:55:46 +0000

    ADd FamousQuotes, hero perk and ropasci

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        [Flags]
+        enum Perks
+        {
+            None = 0,
+            WarpShift = 1 << 0,  // w
+            Stealth = 1 << 1,    // s
+            AutoHeal = 1 << 2,   // a
+            DoubleJump = 1 << 3  // d
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("!Unknown perk!");
+                return;
+            }
+
+            string input = args[0];
+            Perks perks = Perks.None;
+
+            int wCount = 0, aCount = 0, sCount = 0, dCount = 0;
+
+            // Contar quantas vezes cada perk aparece
+            foreach (char c in input)
+            {
+                switch (c)
+                {
+                    case 'w': wCount++; break;
+                    case 'a': aCount++; break;
+                    case 's': sCount++; break;
+                    case 'd': dCount++; break;
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        return;
+                }
+            }
+
+            // Ativar perks se aparecerem um número ímpar de vezes
+            if (wCount % 2 != 0) perks |= Perks.WarpShift;
+            if (aCount % 2 != 0) perks |= Perks.AutoHeal;
+            if (sCount % 2 != 0) perks |= Perks.Stealth;
+            if (dCount % 2 != 0) perks |= Perks.DoubleJump;
+
+            // Exibir perks
+            if (perks == Perks.None)
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+            else
+            {
+                Console.WriteLine(perks);
+            }
+
+            // Mensagens especiais
+            if ((perks & (Perks.Stealth | Perks.DoubleJump)) == (Perks.Stealth | Perks.DoubleJump))
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+
+            if ((perks & Perks.AutoHeal) == 0)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
+        }
+    }
+}
---
+++ RoPaSci/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace RoPaSci
+{
+    class Program
+    {
+        public enum GameItem
+        {
+            Rock,
+            Paper,
+            Scissors
+        }
+
+        public enum GameStatus
+        {
+            Draw,
+            Player1Wins,
+            Player2Wins
+        }
+        private static void Main(string[] args)
+        {
+            
+
+            
+                GameItem player1 = Enum.Parse<GameItem>(args[0], true);
+                GameItem player2 = Enum.Parse<GameItem>(args[1], true);
+
+                GameStatus result = RockPaperScissors(player1, player2);
+
+                switch (result)
+                {
+                    case GameStatus.Draw:
+                        Console.WriteLine("It's a draw!");
+                        break;
+                    case GameStatus.Player1Wins:
+                        Console.WriteLine("Player 1 wins!");
+                        break;
+                    case GameStatus.Player2Wins:
+                        Console.WriteLine("Player 2 wins!");
+                        break;
+                }
+
+        }
+
+        private static GameStatus RockPaperScissors(GameItem player1, GameItem player2)
+        {
+            if (player1 == player2)
+            {
+                return GameStatus.Draw;
+            }
+
+            if ((player1 == GameItem.Rock && player2 == GameItem.Scissors) ||
+                (player1 == GameItem.Scissors && player2 == GameItem.Paper) ||
+                (player1 == GameItem.Paper && player2 == GameItem.Rock))
+            {
+                return GameStatus.Player1Wins;
+            }
+            else
+            {
+                return GameStatus.Player2Wins;
+            }
+        }
+    }
+}
